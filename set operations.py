9752a20cbd0d84933cace7198e09bb0de384d0dc
Harshith Ramesh <80192037+harshith044@users.noreply.github.com>
2021-12-01 22:47:51 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,6 @@
+E={0,1,2,3};
+N={4.5,6,8};
+print("Union of E and N is ",E | N)
+print("Intersection of E and N is ",E & N)
+print("Difference of E and N is ",E -N)
+print("Symmetric differencce of E and N is ",E ^ N)
